91ffd98595a7e7fc4c087bbc530862ece84709d7
Wintersun7 <qiaozhi9551@gmail.com>
2019-11-05 08:42:19 -0500

    Update simulation.m

--- simulation.m
+++ simulation.m
@@ -1,11 +1,8 @@
 %SIMULATION  Start the artificial pancreas simulator GUI.
 
-%% Keep the simulator in the global namespace.
-global simulator;
-
 %% Configure the simulator's paths.
 configurePaths();
 
 %% Start the GUI.
-GUISimulator();
+masseterGUI();
 
